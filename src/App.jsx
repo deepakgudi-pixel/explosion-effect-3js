6fb39c5dab5bbe6402771037028c2f6314956099
Deepak Gudi <japanesen52018@gmail.com>
2023-07-30 20:18:16 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { ScrollControls } from "@react-three/drei";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <Canvas shadows camera={{ position: [0, 0, 5], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
+      <ScrollControls pages={4}>
       <Experience />
+      </ScrollControls>
     </Canvas>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,14 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, Float, OrbitControls } from "@react-three/drei";
+import { Heart } from "./Heart";
+
 
 export const Experience = () => {
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <OrbitControls enableZoom={false}/>
+      <Float floatIntensity={5} speed={3}/>
+       <Heart scale={0.45}/>
+       <Environment preset="sunset" background blur={0.4}/>
     </>
   );
 };
